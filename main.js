6af742f8e0c1ab0c103c054e9f14510197f544c7
Vincent-Maloy <93829782+Vincent-Maloy@users.noreply.github.com>
2022-04-03 14:32:51 -0400

    rfrf

--- main.js
+++ main.js
@@ -2,10 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+<<<<<<< Updated upstream
             image: './assets/images/socks_green.jpg',
             // solution
             url: 'https://www.vuemastery.com/'
             // solution
+=======
+            image: './assets/images/socks_green.jpg'
+>>>>>>> Stashed changes
         }
     }
 })
